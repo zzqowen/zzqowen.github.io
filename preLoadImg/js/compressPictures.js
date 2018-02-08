3c5a5820accc207983ffaa315c4fe170be0a5710
ziqing zhao <1269423219@qq.com>
2018-02-08 16:44:11 +0800

    new change

--- preLoadImg/js/compressPictures.js
+++ preLoadImg/js/compressPictures.js
@@ -69,10 +69,12 @@ var pictures = {
         var xhr = new XMLHttpRequest();
         xhr.open("POST", "/upload", true);
         xhr.send(fd);
-        if(xhr.readyState==4){
-            if(xhr.status==200){
-                console.log(xhr.responseText);
+        xhr.onreadystatechange = function(){
+            if(xhr.readyState==4){
+                if(xhr.status==200){
+                    console.log(xhr.responseText);
+                }
             }
-        }
+        };
     }
 };
